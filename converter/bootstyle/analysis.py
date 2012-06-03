f21f69dc2ff20a409ef9b2e4ee4d9cec6ed5df29
ciembor <pub@ciemborowicz.pl>
2012-06-03 04:53:50 +0200

    Default bootstrap colors.

---
+++ converter/bootstyle/analysis.py
@@ -0,0 +1,8 @@
+def isDark(colors):
+  foreground = colors["special"]["foreground"]
+  background = colors["special"]["background"]
+  if background.getLightness() < foreground.getLightness():
+    return True
+  else:
+    return False
+  
---
+++ converter/bootstyle/bootstrap.py
@@ -0,0 +1,104 @@
+default = {
+                  
+  "scaffolding" : {
+    "@bodyBackground" : "@white",
+    "@textColor" : "@grayDark",
+    "@linkColor" : "#0088cc",
+    "@linkColorHover" : "darken(@linkColor, 15%)"
+  },
+  
+  "tables" : {
+    "@tableBackground" : "transparent",
+    "@tableBackgroundAccent" : "#f9f9f9",
+    "@tableBackgroundHover" : "#f5f5f5",
+    "@tableBorder" : "#dddddd"
+  },
+  
+  "grayscale" : {
+    "@black" : "#000000",
+    "@grayDarker" : "#222222",
+    "@grayDark" : "#333333",
+    "@gray" : "#555555",
+    "@grayLight" : "#999999",
+    "@grayLighter" : "#eeeeee",
+    "@white" : "#ffffff"
+  },
+  
+  "accent" : {
+    "@blue" : "#049cdb",
+    "@green" : "#46a546",
+    "@red" : "#9d261d",
+    "@yellow" : "#ffc40d",
+    "@orange" : "#f89406",
+    "@pink" : "#c3325f",
+    "@purple" : "#7a43b6"
+  },
+
+  "buttons" : {
+    "@btnBackground" : "@white",
+    "@btnBackgroundHighlight" : "darken(@white, 10%)",
+    "@btnBorder" : "darken(@white, 20%)",
+    "@btnPrimaryBackground" : "@linkColor",
+    "@btnPrimaryBackgroundHighlight" : "spin(@btnPrimaryBackground, 15%)",
+    "@btnInfoBackground" : "#5bc0de",
+    "@btnInfoBackgroundHighlight" : "#2f96b4",
+    "@btnSuccessBackground" : "#62c462",
+    "@btnSuccessBackgroundHighlight" : "#51a351",
+    "@btnWarningBackground" : "lighten(@orange, 15%)",
+    "@btnWarningBackgroundHighlight" : "@orange",
+    "@btnDangerBackground" : "#ee5f5b",
+    "@btnDangerBackgroundHighlight" : "#bd362f",
+    "@btnInverseBackground" : "@gray",
+    "@btnInverseBackgroundHighlight" : "@grayDarker"
+  },
+  
+  "forms" : {
+    "@placeholderText" : "@grayLight",
+    "@inputBackground" : "@white",
+    "@inputBorder" : "#ccc",
+    "@inputDisabledBackground" : "@grayLighter",
+    "@formActionsBackground" : "#f5f5f5"
+  },
+  
+  "alerts" : {
+    "@warningText" : "#c09853",
+    "@warningBackground" : "#f3edd2",
+    "@errorText" : "#b94a48",
+    "@errorBackground" : "#f2dede",
+    "@successText" : "#468847",
+    "@successBackground" : "#dff0d8",
+    "@infoText" : "#3a87ad",
+    "@infoBackground" : "#d9edf7"
+  },
+
+  "navbar" : {
+    "@navbarBackground" : "@grayDarker",
+    "@navbarBackgroundHighlight" : "@grayDark",
+    "@navbarText" : "@grayLight",
+    "@navbarLinkColor" : "@grayLight",
+    "@navbarLinkColorHover" : "@white",
+    "@navbarLinkBackgroundHover" : "transparent",
+    "@navbarLinkBackgroundActive" : "@navbarBackground",
+    "@navbarSearchBackground" : "lighten(@navbarBackground, 25%)",
+    "@navbarSearchBackgroundFocus" : "@white",
+    "@navbarSearchBorder" : "darken(@navbarSearchBackground, 30%)",
+    "@navbarSearchPlaceholderColor" : "darken(@white, 20%)",
+    "@navbarBrandColor" : "@navbarLinkColor"
+  },
+  
+  "dropdown" : {
+    "@dropdownBackground" : "@white",
+    "@dropdownBorder" : "rgba(0,0,0,.2)",
+    "@dropdownLinkColor" : "@grayDark",
+    "@dropdownLinkColorHover" : "@white",
+    "@dropdownLinkBackgroundHover" : "@linkColor",
+    "@dropdownDividerTop" : "#e5e5e5",
+    "@dropdownDividerBottom" : "@white"
+  },
+  
+  "herounit" : {
+    "@heroUnitBackground" : "@grayLighter",
+    "@heroUnitHeadingColor" : "inherit",
+    "@heroUnitLeadColor" : "inherit"
+  }
+}
